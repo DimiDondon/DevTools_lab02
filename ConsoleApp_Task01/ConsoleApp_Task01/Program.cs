e5654651df2da8ebf5181515dba5ce8bf17bfe39
DimiDondon <mihailovdima2.0@mail.ru>
2023-02-16 22:24:45 +0400

    add files

--- ConsoleApp_Task01/ConsoleApp_Task01/Program.cs
+++ ConsoleApp_Task01/ConsoleApp_Task01/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using Wintellect.PowerCollections;
+
+Wintellect.PowerCollections.Stack<char> stack = new();
+
+stack.Push('f');
+stack.Push('c');
+
+Console.WriteLine("Результат " + stack.Pop() + " " + stack.Capacity + " " + stack.Count);
